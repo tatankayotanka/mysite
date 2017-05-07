7860f9c11a2e4b9c1acebac3f3ec5c11a99e1294
evazhouqiao@gmail.com <evazhouqiao@gmail.com>
2017-05-07 13:04:26 +0200

    make mainpage persistent

--- task/views.py
+++ task/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Task
 #views are functions that usually return a HTTP response
 def index(request):
     all_tasks = Task.objects.all()
-    template = loader.get_template('task/index.html')
+    #template = loader.get_template('task/index.html')
     context = {
         'all_tasks':all_tasks,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'task/index.html',context)
 
 def detail(request, task_id):
-    return HttpResponse("<h2>Details for Album " +str(task_id) +"</h2>")
+    return HttpResponse("<h2>Details for Task " +str(task_id) +"</h2>")
